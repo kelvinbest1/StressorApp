a796b7b9c4e1e4a5872136b699c7302d1c1a8dc9
Kelvin <60621382+kelvinbest1@users.noreply.github.com>
2021-03-17 15:35:42 -0400

    updated backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
   resources :stressors
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+end
 end
