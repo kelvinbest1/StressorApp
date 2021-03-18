5829897264bd2d4b2ea6637a352e31eaa4c6aeb6
Kelvin <60621382+kelvinbest1@users.noreply.github.com>
2021-03-18 18:47:33 -0400

    updated User table

--- db/migrate/20210316193415_create_stressors.rb
+++ db/migrate/20210316193415_create_stressors.rb
@@ -2,7 +2,7 @@ class CreateStressors < ActiveRecord::Migration[6.1]
   def change
     create_table :stressors do |t|
       t.string :name
-      t.boolean :conquered
+      t.boolean :overcamed
       t.integer :user_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_03_16_193415) do
 
   create_table "stressors", force: :cascade do |t|
     t.string "name"
-    t.boolean "conquered"
+    t.boolean "overcamed"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
