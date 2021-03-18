96a081ba240b00f04e3d2417aa468aecb880cf04
Kelvin <60621382+kelvinbest1@users.noreply.github.com>
2021-03-18 11:43:05 -0400

    added fetch.js

---
+++ frontend/src/fetch.js
@@ -0,0 +1,6 @@
+class FetchCalls {
+    constructor () {
+    this.baseURL = "http://localhost:3000/api/v1"
+    this.userURL = `${this.baseURL}/users`
+    this.goalURL = `${this.baseURL}/goals`
+    }
