cd4b07f39b08887deb1f8e27ba2b89637ad26d0c
Kelvin <60621382+kelvinbest1@users.noreply.github.com>
2021-03-18 18:58:19 -0400

    added user's stressor render

--- frontend/src/stressors.js
+++ frontend/src/stressors.js
@@ -10,3 +10,11 @@ class Stressor {
         this.user_id = stressor.user_id
         Goal.userStressors.push(stressor)
     }
+
+     //this will render each of the user's stressors
+     renderStressors(){
+        let stressorcontainer = document.querySelector(".stressors-container");
+        let stressorp = document.createElement('p');
+        stressorp.innerText = this.name;
+        stressorp.id = `t${this.id}`;
+        stressorcontainer.appendChild(stressorp);
