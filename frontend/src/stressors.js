12248e9d0db41ffe72249b48a9287ae40491f9b9
Kelvin <60621382+kelvinbest1@users.noreply.github.com>
2021-03-18 18:54:16 -0400

    added constructor

--- frontend/src/stressors.js
+++ frontend/src/stressors.js
@@ -1,3 +1,12 @@
 class Stressor {
 
     static userStressors = []
+
+
+    constructor(stressor){
+        this.name = stressor.name
+        this.id = stressor.id
+        this.overcamed = stressor.overcamed
+        this.user_id = stressor.user_id
+        Goal.userStressors.push(stressor)
+    }
