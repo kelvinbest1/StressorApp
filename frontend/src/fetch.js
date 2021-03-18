bafdc502f8a6c64c14289e15738a2288f9641a08
Kelvin <60621382+kelvinbest1@users.noreply.github.com>
2021-03-18 15:07:05 -0400

    added fetch

--- frontend/src/fetch.js
+++ frontend/src/fetch.js
@@ -1,6 +1,26 @@
 class FetchCalls {
     constructor () {
-    this.baseURL = "http://localhost:3000/api/v1"
+    this.baseURL = "http://localhost:3000/"
     this.userURL = `${this.baseURL}/users`
     this.goalURL = `${this.baseURL}/goals`
+    }
+
+
+    //this will create or find a user
+    async findOrCreateUser(e){
+        const resp = await fetch(this.userURL, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(
+          {
+            users: {
+              name: e.target.children[1].value
+            }
+          })
+      })
+      let json = resp.json();
+      return await json;
     }
