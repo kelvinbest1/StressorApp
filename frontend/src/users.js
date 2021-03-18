d4e2178fb6dfe06cb3e36049be1da63cc4eb1505
Kelvin <60621382+kelvinbest1@users.noreply.github.com>
2021-03-18 18:25:15 -0400

    completed UserStressors

--- frontend/src/users.js
+++ frontend/src/users.js
@@ -3,3 +3,33 @@ class User {
         this.name = user.name
         this.id = user.id
     }
+
+     //This will login the user into the site
+     static login(){
+        let username = document.getElementById('user-form');
+        username.addEventListener("submit" , function(e){ 
+            e.preventDefault();
+            if (e.target.children[1].value != "") {
+                fetchCall.findOrCreateUser(e)
+                .then(user => {
+                    let newUser = new User(user);
+                    username.reset();
+                    newUser.displayUserStressors();
+                })
+            }
+        })
+    }   
+
+    //This will get all the user's stressors and display them
+    displayUserStressors(){
+        Goal.stressorForm(this.name);
+        fetchCall.getAllStressors()
+        .then(stressors => {
+            for (let i=0; i < stressors.length; i++){
+                if (this.id == stressors[i].user_id){
+                    let personalStressor = new Stressor(stressors[i]);
+                    personalStressor.renderStressors();
+                }
+            }
+        
+    
