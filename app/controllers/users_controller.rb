57410b6728bed0071f904f9c3be225c6767d317f
Kelvin <60621382+kelvinbest1@users.noreply.github.com>
2021-03-18 23:34:39 -0400

    updated controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,20 @@ class UsersController < ApplicationController
     render json: users
 end
 
+# POST /users
+def create
+  # binding.pry
+   user = User.find_by(name: params[:users][:name])
+   if user == nil
+       user = User.create(user_params)
+       user.save
+       render json: user
+   else
+       redirect_to "users/#{user.id}"
+   end
+end
+
+
   # GET /users/1
   def show
     user = User.find_by(id: params[:id])
@@ -14,19 +28,7 @@ end
 end
 
 
-  # POST /users
-  def create
-    # binding.pry
-     user = User.find_by(name: params[:users][:name])
-     if user == nil
-         user = User.create(user_params)
-         user.save
-         render json: user
-     else
-         redirect_to "users/#{user.id}"
-     end
- end
-
+  
 
   # PATCH/PUT /users/1
   def update
@@ -46,6 +48,6 @@ end
    
      #Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name)
+      params.fetch(:user).permit(:name)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "Samuel")
+Stressor.create(name: 'children', overcamed: false, user_id: 2)
